4264840bd6ffb90ab82b11490d775af5c186a612
artem <miroshnichenkoartem0307@gmail.com>
2025-03-10 15:20:14 +0100

    add currency models

--- currency/models.py
+++ currency/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+
+    class Meta:
+        db_table = 'currency'
+
+    def __str__(self):
+        return self.code
+
+
+class CurrencyRateHistory(models.Model):
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    per_usd = models.DecimalField(max_digits=14, decimal_places=7)
+    date = models.DateTimeField()
+
+    class Meta:
+        get_latest_by = ['date']
+        db_table = 'currency_rate_history'
+
+    def __str__(self):
+        return f'{self.id}: {self.per_usd}'
+
