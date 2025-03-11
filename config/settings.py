53ee75f9c03181bc4d6a6e5076d81fda53f6bc88
artem <miroshnichenkoartem0307@gmail.com>
2025-03-11 01:39:32 +0100

    add django admin

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ ALLOWED_HOSTS = ['*'] if DEBUG else []
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
