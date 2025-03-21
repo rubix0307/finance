df701ccad17fc94cff1f7a5d20b8c4a9fb277962
artem <miroshnichenkoartem0307@gmail.com>
2025-03-21 21:38:08 +0100

    updated Shop.find_or_create

--- receipt/models.py
+++ receipt/models.py
@@ -1,12 +1,11 @@
-from typing import Optional, cast, Annotated, Any, LiteralString, Protocol, runtime_checkable
+from typing import Optional, cast, Annotated
 
-from django.contrib.auth import get_user_model
 from django.contrib.postgres.search import TrigramSimilarity
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from currency.models import Currency
 from .common import get_photo_upload_path
-
 
 class Shop(models.Model):
     name = models.CharField(max_length=255, unique=True, db_index=True)
@@ -16,29 +15,46 @@ class Shop(models.Model):
     objects = models.Manager()
 
     def __str__(self) -> str:
-        return f'{self.name}{f'- {self.address}' if self.address else ''}'
+        return f'{self.name}{f"- {self.address}" if self.address else ""}'
 
     @classmethod
-    def find_or_create(cls, name: str, address: Optional[str] = None, similarity: Annotated[float, 'from 0 to 1.0'] = 0.7) -> 'Shop':
-        """
-        Quickly search for a similar store. If not found, create a new one.
-        Similarity search via TrigramSimilarity (PostgreSQL) is used.
-        """
-
+    def find_or_create(cls,
+        name: str,
+        address: Optional[str] = None,
+        taxpayer_id: str | None = None,
+        similarity: Annotated[float, 'from 0 to 1.0'] = 0.7
+    ) -> 'Shop':
         normalized_name = name.strip().lower()
-        similar_shop = cls.objects.annotate(
-            similarity=TrigramSimilarity('name', normalized_name)
-        ).filter(similarity__gt=similarity).order_by('-similarity').first()
 
-        if similar_shop:
-            return cast(Shop, similar_shop)
+        if taxpayer_id:
+            similar_shop = cls.objects.annotate(
+                similarity=TrigramSimilarity('name', normalized_name)
+            ).filter(
+                taxpayer_id=taxpayer_id,
+                similarity__gte=0.95
+            ).order_by('-similarity').first()
+            if similar_shop:
+                return cast(Shop, similar_shop)
 
-        shop, _ = cls.objects.get_or_create(name=name, defaults={"address": address})
+        else:
+            similar_shop = cls.objects.annotate(
+                similarity=TrigramSimilarity('name', normalized_name)
+            ).filter(
+                taxpayer_id__isnull=True,
+                similarity__gte=similarity
+            ).order_by('-similarity').first()
+            if similar_shop:
+                return cast(Shop, similar_shop)
+
+        shop, _ = cls.objects.get_or_create(
+            name=name,
+            taxpayer_id=taxpayer_id,
+            defaults={"address": address}
+        )
         return cast(Shop, shop)
 
     class Meta:
         db_table = 'shop'
-
 
 class Receipt(models.Model):
     photo = models.ImageField(upload_to=get_photo_upload_path, null=True, blank=True, max_length=1024)
