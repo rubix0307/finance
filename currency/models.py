000c75a7db4610dbfc9f8c340c6ede32f04c3635
artem <miroshnichenkoartem0307@gmail.com>
2025-03-30 13:09:49 +0200

    added symbol to Currency

--- currency/models.py
+++ currency/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Currency(models.Model):
     code = models.CharField(max_length=3, unique=True, primary_key=True)
+    symbol = models.CharField(max_length=3, null=True, blank=True)
 
     class Meta:
         db_table = 'currency'
