dba426c8c2a4c9c3b7ae4588d53c67b1c6a3bf5c
artem <miroshnichenkoartem0307@gmail.com>
2025-04-09 23:33:04 +0200

    added USD rate to CurrencyRateHistory

--- currency/apps.py
+++ currency/apps.py
@@ -1,6 +1,23 @@
+from datetime import datetime
 from django.apps import AppConfig
+
 
 
 class CurrencyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'currency'
+
+    def ready(self):
+        from .models import CurrencyRateHistory, Currency
+
+        try:
+            usd_currency = Currency.objects.get(code='USD')
+        except Currency.DoesNotExist:
+            raise RuntimeError("USD currency must exist")
+
+        if not CurrencyRateHistory.objects.filter(currency=usd_currency).exists():
+            CurrencyRateHistory.objects.create(
+                currency=usd_currency,
+                per_usd=1,
+                date=datetime.now(),
+            )
