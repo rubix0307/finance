0ad334d46e3a954b6abeb0d800045f39695b72f0
artem <miroshnichenkoartem0307@gmail.com>
2025-04-10 01:30:58 +0200

    added checking settings.DEBUG

--- currency/apps.py
+++ currency/apps.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.apps import AppConfig
 
+from django.conf import settings
 
 
 class CurrencyConfig(AppConfig):
@@ -9,15 +10,15 @@ class CurrencyConfig(AppConfig):
 
     def ready(self):
         from .models import CurrencyRateHistory, Currency
+        if not settings.DEBUG:
+            try:
+                usd_currency = Currency.objects.get(code='USD')
+            except Currency.DoesNotExist:
+                raise RuntimeError("USD currency must exist")
 
-        try:
-            usd_currency = Currency.objects.get(code='USD')
-        except Currency.DoesNotExist:
-            raise RuntimeError("USD currency must exist")
-
-        if not CurrencyRateHistory.objects.filter(currency=usd_currency).exists():
-            CurrencyRateHistory.objects.create(
-                currency=usd_currency,
-                per_usd=1,
-                date=datetime.now(),
-            )
+            if not CurrencyRateHistory.objects.filter(currency=usd_currency).exists():
+                CurrencyRateHistory.objects.create(
+                    currency=usd_currency,
+                    per_usd=1,
+                    date=datetime.now(),
+                )
