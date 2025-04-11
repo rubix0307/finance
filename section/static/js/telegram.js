87a8de4378e47a9b5d26c362d4be8cd7a7b425f1
artem <miroshnichenkoartem0307@gmail.com>
2025-04-11 13:17:31 +0200

    added set webApp header color

--- section/static/js/telegram.js
+++ section/static/js/telegram.js
@@ -1,30 +1,29 @@
 class TelegramAppHelper {
-    constructor() {
-        window.addEventListener('DOMContentLoaded', () => {
-            this.webApp = window.Telegram?.WebApp;
+  constructor(webApp) {
+    this.webApp = webApp;
+    this.webApp.expand();
+  }
 
-            if (!this.webApp) {
-                console.warn("Telegram WebApp не найден.");
-                return;
-            }
+  setHeaderColorFromVar(varName) {
+    const root = document.documentElement;
+    const color = getComputedStyle(root).getPropertyValue(varName).trim();
 
-            this.webApp.expand();
-        });
+    if (color) {
+      this.webApp.setHeaderColor(color);
     }
-
-    setHeaderColorFromVar(varName) {
-        if (!this.webApp) return;
-
-        const root = document.documentElement;
-        const color = getComputedStyle(root).getPropertyValue(varName).trim();
-
-        if (color) {
-            this.webApp.setHeaderColor(color);
-        }
-    }
+  }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    const tgHelper = new TelegramAppHelper();
-    tgHelper.setHeaderColorFromVar('--main_purple');
-})
+  const webApp = window.Telegram?.WebApp;
+
+  if (!webApp) {
+    console.warn("Telegram.WebApp not found");
+    return;
+  }
+
+  const tgHelper = new TelegramAppHelper(webApp);
+  tgHelper.setHeaderColorFromVar('--main_purple');
+
+});
+
