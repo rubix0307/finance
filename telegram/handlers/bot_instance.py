d40e88c2459d92cdf5ebbc0b15626935a339916a
artem <miroshnichenkoartem0307@gmail.com>
2025-04-13 13:48:11 +0200

    added run bot with infinity_polling (for dev)

--- telegram/handlers/bot_instance.py
+++ telegram/handlers/bot_instance.py
@@ -1,4 +1,11 @@
+import os
+from threading import Thread
+
 import telebot
 from django.conf import settings
 
 bot = telebot.TeleBot(settings.TELEGRAM_BOT_TOKEN)
+
+if settings.DEBUG and os.getenv('TELEGRAM_BOT_INFINITY_POLLING') == '1':
+    bot.remove_webhook()
+    Thread(target=bot.infinity_polling).start()
