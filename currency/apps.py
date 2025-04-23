340f897ce6c5a251b0f861235b0673c5937f16cb
artem <miroshnichenkoartem0307@gmail.com>
2025-04-23 22:31:46 +0200

    remove to query before start app

--- currency/apps.py
+++ currency/apps.py
@@ -7,18 +7,3 @@ from django.conf import settings
 class CurrencyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'currency'
-
-    def ready(self):
-        from .models import CurrencyRateHistory, Currency
-        if not settings.DEBUG:
-            try:
-                usd_currency = Currency.objects.get(code='USD')
-            except Currency.DoesNotExist:
-                raise RuntimeError("USD currency must exist")
-
-            if not CurrencyRateHistory.objects.filter(currency=usd_currency).exists():
-                CurrencyRateHistory.objects.create(
-                    currency=usd_currency,
-                    per_usd=1,
-                    date=datetime.now(),
-                )
