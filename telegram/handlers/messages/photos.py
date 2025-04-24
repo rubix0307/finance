9aa8c7a4cc459e36b33aa45035a0da7e89cb4d07
artem <miroshnichenkoartem0307@gmail.com>
2025-04-24 15:14:34 +0200

    added markup for completed message

--- telegram/handlers/messages/photos.py
+++ telegram/handlers/messages/photos.py
@@ -3,10 +3,11 @@ from typing import Any
 from django.core.files.base import ContentFile
 from django.utils import translation
 from django.utils.translation import gettext as _
-from telebot.types import LabeledPrice, Message
+from telebot.types import LabeledPrice, Message, InlineKeyboardMarkup
 
 from receipt.models import Receipt
 from telegram.handlers.bot_instance import bot
+from telegram.handlers.common import ButtonStorage
 from telegram.handlers.utils import user_required
 from telegram.models import ReceiptStatusMessage, Status
 from user.models import User
@@ -15,8 +16,6 @@ from user.models import User
 @bot.message_handler(content_types=['photo'])
 @user_required
 def get_photo(message: Message, user: User, **kwargs: dict[str, Any]) -> None:
-
-
     photo = message.photo[-1]
     file_info = bot.get_file(photo.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
@@ -46,10 +45,14 @@ def get_photo(message: Message, user: User, **kwargs: dict[str, Any]) -> None:
 def update_message(receipt_status: ReceiptStatusMessage, **kwargs: dict[str, Any]) -> None:
     with translation.override(receipt_status.receipt.owner.language_code):
         caption = None
+        markup = None
         if receipt_status.status == Status.IN_PROGRESS:
             caption = _('In progress')
         elif receipt_status.status == Status.PROCESSED:
             caption = _('Analysis completed')
+            markup = InlineKeyboardMarkup()
+            markup.add(ButtonStorage.web_app_main())
+
         elif receipt_status.status == Status.ERROR:
             caption = _('Analysis error')
 
@@ -59,6 +62,7 @@ def update_message(receipt_status: ReceiptStatusMessage, **kwargs: dict[str, Any
                     caption=caption,
                     chat_id=receipt_status.chat_id,
                     message_id=receipt_status.message_id,
+                    reply_markup=markup,
                 )
             except Exception as ex:
                 ...
