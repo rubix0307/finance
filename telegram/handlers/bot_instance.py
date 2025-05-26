655dc8518bf11108d4e82331d28f4331c359f780
artem <miroshnichenkoartem0307@gmail.com>
2025-05-26 22:45:16 +0200

    change start bot pooling

--- telegram/handlers/bot_instance.py
+++ telegram/handlers/bot_instance.py
@@ -6,10 +6,9 @@ from django.conf import settings
 
 bot = telebot.TeleBot(settings.TELEGRAM_BOT_TOKEN)
 
-if settings.DEBUG and os.getenv('TELEGRAM_BOT_INFINITY_POLLING') == '1':
-    start = input('Run bot pooling y/n')
-    if start == 'y':
-        bot.remove_webhook()
-        Thread(target=lambda: bot.infinity_polling(
-            allowed_updates=['message', 'edited_message', 'callback_query','pre_checkout_query', 'successful_payment']
-        )).start()
+if os.getenv('run_bot') == '1':
+    os.environ['run_bot'] = '0'
+    bot.remove_webhook()
+    Thread(target=lambda: bot.infinity_polling(
+        allowed_updates=['message', 'edited_message', 'callback_query','pre_checkout_query', 'successful_payment']
+    )).start()
