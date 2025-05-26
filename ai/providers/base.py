16950097c48606c943d21c9cc8e3e192d5b5841b
artem <miroshnichenkoartem0307@gmail.com>
2025-05-26 22:44:57 +0200

    remove providers

--- ai/providers/base.py
+++
@@ -1,8 +0,0 @@
-from abc import abstractmethod, ABC
-from ai.managers.schemas import ProcessPhotoResponse
-
-
-class AIProvider(ABC):
-    @abstractmethod
-    def process_photo(self, photo: str) -> ProcessPhotoResponse:
-        pass
