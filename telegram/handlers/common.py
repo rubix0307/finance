35312176865d2def06ce93295efeac7ee173f232
artem <miroshnichenkoartem0307@gmail.com>
2025-09-16 21:24:13 +0300

    change static url to settings.BASE_URL

--- telegram/handlers/common.py
+++ telegram/handlers/common.py
@@ -1,4 +1,6 @@
 from typing import Any, Callable
+
+from django.conf import settings
 from django.utils.translation import gettext as _
 from telebot.callback_data import CallbackData, CallbackDataFilter
 from telebot.types import CallbackQuery, InlineKeyboardButton as IKB, Message, WebAppInfo
@@ -17,7 +19,7 @@ class CallbackStorage:
 
 class ButtonStorage:
     web_app_main = lambda label=None: IKB(label or _('Open the app'), url='https://t.me/finance_lens_bot?startapp')
-    web_app_faq = lambda label=None: IKB(label or 'FAQ', web_app=WebAppInfo(url='https://finance-lens.online/telegram/check-web-app/?next=/faq/'))
+    web_app_faq = lambda label=None: IKB(label or 'FAQ', web_app=WebAppInfo(url=f'{settings.BASE_URL}/telegram/check-web-app/?next=/faq/'))
     menu_start = lambda label=None: IKB(label or _('Main page'), callback_data=CallbackStorage.menu.new(name='start'))
     menu_language = lambda label=None: IKB(label or '🌐 ' + _('Language'), callback_data=CallbackStorage.menu.new(name='language'))
 
